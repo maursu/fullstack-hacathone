3576c540336c575bf256c60abfc51e435728ca6f
Александр Маурус <alexmaurus@MacBook-Pro-MSP.local>
2023-02-12 20:43:56 +0600

    created user model, changed some settings module, added some stuff to account app

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,11 @@ AUTH_USER_MODEL = 'account.User'
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = True
@@ -105,6 +110,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 
 # Default primary key field type
